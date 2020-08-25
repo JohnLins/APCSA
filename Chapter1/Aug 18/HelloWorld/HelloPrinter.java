293fbd3b9d33b66ebf022b2fb2c150a9e0ce504d
John Lins <46113320+JohnLins@users.noreply.github.com>
2020-08-25 10:33:56 -0700

    Add files via upload

---
+++ Chapter1/Aug 18/HelloWorld/HelloPrinter.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class HelloPrinter here.
+ *
+ * @author (John Lins)
+ * @version (Tus, Aug 18, 2020)
+ */
+public class HelloPrinter
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, John!");
+    }
+}
---
+++ Chapter1/Aug 18/Variables/HelloPrinter.java
@@ -0,0 +1,19 @@
+/**
+ * Write a description of class var here.
+ *
+ * @author (John Lins)
+ * @version (Tus, Aug 18, 2020)
+ */
+public class HelloPrinter
+{
+    public static void main(String[] args)
+    {
+        int x = 7;
+        int y = 9;
+        
+        int z;
+        z = x + y;
+        System.out.print("x + y = ");
+        System.out.print(z);
+    }
+}
