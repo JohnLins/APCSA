bb9d72d49597602c6432e8290ac41ccd69764885
John Lins <johnpaullins@gmail.com>
2021-03-09 11:24:50 -0800

    Mar9

---
+++ Chaper10/mar9/Exeptions.java
@@ -0,0 +1,10 @@
+public class Exeptions {
+  public static void main(String[ ] args) {
+    try {
+      int[] arr = {1, 2, 3};
+      System.out.println(arr[5]);
+    } catch (Exception e) {
+      System.out.println("Something went wrong.");
+    }
+  }
+}
